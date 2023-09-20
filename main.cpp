97aaadebb8fed447fe87e1cd852281875aed7758
antAlexandr <aa.antipov05@gmail.com>
2023-09-21 00:29:33 +0300

    add cdtor.h

--- main.cpp
+++ main.cpp
@@ -3,14 +3,20 @@
 #include <assert.h>
 
 #include "error.h"
+#include "cdtor.h"
 
 int main()
 {
-    struct Stack stk = {.data = 0, .size = 5, .capacity = 8};
-    stk.data = (int*) calloc (stk.capacity, sizeof (int));
-    assert (stk.data);
+    struct Stack stk = {};
+
+    if (!StackCtor(&stk)) return 0;
+    stk.data[stk.size++] = 5;
 
     StackDump (&stk, __FILE__, __LINE__);
 
+    StackDtor (&stk);
+
     return 0;
 }
+
+//int Push
